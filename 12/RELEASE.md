2eeb20506f5e689b30d80b26d0dda014ba44bee3
juanjmerono <jjmerono@um.es>
2017-09-05 16:32:01 +0200

    Initial import for 12 Release just copy from 11 and update to 12

---
+++ 12/RELEASE.md
@@ -0,0 +1,104 @@
+Work in Progress for doing the 12 release
+
+## Command line release in the git repository
+
+```
+# I like to tmux before I do any of this so I can reattach to it later since this takes a long time, especially on the deploy
+tmux new -s release
+
+# Change this each release
+export SAKAI_VERSION=12.0
+
+#This should be static
+export SAKAI_SNAPSHOT_VERSION=12-SNAPSHOT
+
+git clone git@github.com:sakaiproject/sakai.git sakai-source-release
+# Or  git pull --rebase  if you're in an already existing directory
+cd sakai-source-release
+git fetch
+git checkout 12.x
+
+# Now that you're in the 12 branch, setup the repo to release!
+cd master
+#First run the plugin to process and set the version
+mvn versions:set -DnewVersion=${SAKAI_VERSION} -DgenerateBackupPoms=false
+#Then fix up the SNAPSHOT in the properties
+sed -i -e "s/${SAKAI_SNAPSHOT_VERSION}/${SAKAI_VERSION}/" pom.xml
+cd ..
+
+#Release all the needed binaries to the repo
+# The skipLocalStaging here is optional, and probably better to leave it off then it deploys all at the end
+# Also you might need -DstagingRepositoryId=orgsakaiproject-1059
+# Where the id is whatever id you've previously deployed into, if you're adding additional artifacts
+
+
+mvn deploy -Dsakai-release=true -Dmaven.test.skip=true -DskipLocalStaging=true
+
+#Build Sakai and the packs
+mvn clean install -Ppack-bin -Dmaven.test.skip=true
+
+#Now do the necessary commits to git with the tag if everything's completed successfully so far
+git commit -a -m "Releasing Sakai ${SAKAI_VERSION}"
+git tag -a ${SAKAI_VERSION} -m "Tagging Sakai version ${SAKAI_VERSION}"
+
+cd master
+mvn versions:set -DnewVersion=${SAKAI_SNAPSHOT_VERSION} -DgenerateBackupPoms=false
+#Then fix up the SNAPSHOT in the properties
+sed -i -e "s/${SAKAI_VERSION}/${SAKAI_SNAPSHOT_VERSION}/" pom.xml 
+cd ..
+git commit -a -m "Switching Sakai back to ${SAKAI_SNAPSHOT_VERSION}"
+```
+
+## Checking sonatype and releasing artifacts
+
+Now if everythings okay (examine the git log, check sonatype close/release the artifacts there at https://oss.sonatype.org/index.html#welcome), push it!
+
+If for some reason closing the artifacts doesn't work (like maven it fails a rule) you can try to re-deploy to the same repository by using the -DstagingRepositoryId parameter like below. Remember you might have to checkout the tag again to work on that, since you might be already at the SNAPSHOT phase.
+
+```
+mvn deploy -Dsakai-release=true -Dmaven.test.skip=true -DskipLocalStaging=true -DstagingRepositoryId=orgsakaiproject-1068
+```
+
+You can also delete and try again back on the deploy step.
+
+```
+git push origin ${SAKAI_VERSION}
+git push origin 12.x
+
+```
+
+## Deploying binaries and javadocs
+
+Afterward you should generate the javadocs upload all of the artifacts.
+
+You have to have an alias in .ssh/config to the sakai static release directory for the command below to work. Otherwise set one up or have something comparable.
+
+Because this hits the server multiple times you might also want to use an app like keychain or ssh-agent.
+
+First clean up and make the directory you'll need
+
+`ssh sakaistatic "rm -rf ~/public_html/release/${SAKAI_VERSION}/artifacts ~/public_html/release/${SAKAI_VERSION}/apidocs"` 
+
+`ssh sakaistatic "mkdir -p ~/public_html/release/${SAKAI_VERSION}/artifacts ~/public_html/release/${SAKAI_VERSION}/apidocs"` 
+
+Then you can copy the files over
+
+`cd pack ; find . -name "*sakai-*" | xargs -I {} scp {} sakaistatic:~/public_html/release/${SAKAI_VERSION}/artifacts; cd ..`
+
+Finally generate the javadocs
+
+Checkout 12.3 again so the docs are right
+`git checkout 12.3`
+
+Run this in the top level directory to generate and aggregate the java docs
+`mvn javadoc:aggregate`
+
+And then upload them to the release directory (Make sure it's empty)
+`rsync -r target/site/apidocs sakaistatic:~/public_html/release/${SAKAI_VERSION}/`
+
+And checkout 12.x again
+`git checkout 12.x`
+
+* And that should be it! Close the Jira, check the release page for working links and send out the release notes! *
+
+These have only been run a few times, so hopefully they work for you. Will update this next release cycle!
---
+++ 12/index.js
@@ -0,0 +1,264 @@
+#!/usr/bin/env node
+var program = require('commander');
+var exec = require('child_process').exec;
+var chalk = require('chalk');
+
+var querystring = require('querystring');
+var https = require('https');
+
+function performRequest(host, endpoint, method, data, success) {
+  var dataString = JSON.stringify(data);
+  var headers = {};
+  
+  if (method == 'GET') {
+    endpoint += '?' + querystring.stringify(data);
+  } else {
+    headers = {
+      'Content-Type': 'application/json',
+      'Content-Length': dataString.length
+    };
+  }
+  var options = {
+    host: host,
+    path: endpoint,
+    method: method,
+    headers: headers
+  };
+
+  var req = https.request(options, function(res) {
+    res.setEncoding('utf-8');
+    var responseString = '';
+    res.on('data', function(data) {
+      responseString += data;
+    });
+    res.on('end', function() {
+      var responseObject = JSON.parse(responseString);
+      success(responseObject);
+    });
+  });
+
+  req.write(dataString);
+  req.end();
+}
+
+var processLines = function(stdout,filter) {
+	var outLines = stdout.split(/\r?\n/);
+	var filterLines = [];
+	for (var i=0; i<outLines.length; i++) {
+		if (filter(outLines[i])) {
+			filterLines.push(outLines[i]);
+		}
+	}
+	return filterLines;
+}
+
+var extractJiraKeys = function(t) {
+	var jira_matcher = /\d+-[A-Z]+(?!-?[a-zA-Z]{1,10})/g	 
+	var s = t.split("").reverse().join("");
+	var m = s.match(jira_matcher);
+	if (!m) return [];
+	// Also need to reverse all the results!
+	for (var i = 0; i < m.length; i++) {
+		m[i] = m[i].split("").reverse().join("");
+	}
+	m.reverse();
+	return m;
+}
+
+var isJiraRelated = function(t) {
+	return extractJiraKeys(t).length > 0;
+}
+
+var isGithubRelated = function(t) {
+	return !isJiraRelated(t) && t.substring(12).match(/[^(]#*[0-9]{4,}[^)]/);
+}
+
+var isOnly = function(t) {
+	return !program.only || t.indexOf(program.only) >= 0;
+}
+
+var jiralist = new Array();
+var remainingJiras = 0;
+var checkJiraApi = function(key,success) {
+	if (!jiralist[key]) {
+		jiralist[key] = { status:'', merge: '' }
+		remainingJiras++;
+		performRequest('jira.sakaiproject.org','/rest/api/2/issue/'+key,'GET',{fields:['status','customfield_11676']},function(data){
+			var status = 'none', merge = 'none';
+			if (!data.errorMessages) {
+				if (data.fields.status) status = data.fields.status.name;
+				if (data.fields.customfield_11676) merge = data.fields.customfield_11676.value;
+				jiralist[key].status = status;
+				jiralist[key].merge = merge;
+			} else {
+				jiralist[key].status = 'unknown';
+				jiralist[key].merge = 'unknown';
+			}
+			remainingJiras--;
+			if (remainingJiras==0) {
+				success(jiralist);
+			}
+		});
+	}
+}
+
+var isJiraReady = function(t,f) {
+	var keys = extractJiraKeys(t);
+	for (var k=0; k<keys.length; k++) {
+		checkJiraApi(keys[k],f);
+	}
+	return true;
+}
+
+var checkJira = function(key,v) {
+	return (v[1] == 'any' || jiralist[key].status.toLowerCase() == v[1]) && (v[2] == jiralist[key].merge.toLowerCase());
+}
+
+var areJirasReady = function(keys,v) {
+	var ready = true;
+	for (var k=0; k<keys.length; k++) {
+		if (!jiralist[keys[k]] || !checkJira(keys[k],v)) {
+			ready = false;
+		}
+	}
+	return ready;
+}
+
+var showLine = function(line,ignore) {
+	if (program.picks && !ignore) {
+		console.log(chalk.red('git cherry-pick '+line.substring(2,12),chalk.green(' //'+line.substring(13))));
+	} else {
+		console.log(chalk.cyan(line));
+	}
+}
+
+var showLines = function(message,lines,ignore) {
+	console.log(message);
+	for (var i=0; i<lines.length; i++) {
+		showLine(lines[i],ignore);
+	}
+}
+
+var processOptions = function(updated) {
+	if (program.find) {
+		console.log('Searching for commit \''+program.find+'\' in 12.x branch...')
+		exec((!updated?"git checkout 12.x;":"")+"git log --pretty=oneline --since=\"2016-02-17\"", function (error, stdout, stderr) {
+			var flines = processLines(stdout,function(t) { return t.indexOf(program.find) >= 0 });
+			if (flines.length==0) { console.log('Commit \''+program.find+'\' not found in 12.x branch!'); }
+			else {
+				showLines('Found \''+program.find+'\' in 12.x branch',flines,true);
+			}
+		})
+	} else {
+		exec("git checkout 12.x",function (error, stdout, stderr) {
+			if (error) {
+				console.log(stderr);
+				return;
+			}
+			if (program.missing) {
+				console.log('Searching for missing commits...');
+				exec("git cherry 12.x master -v --abbrev=10", function (error, stdout, stderr) {
+					var flines = processLines(stdout,function(t) { 
+						return t.indexOf('+') == 0 
+							&& !isJiraRelated(t)
+							&& !isGithubRelated(t)
+							&& isOnly(t);
+					});
+					showLines('Found '+flines.length+' missed commits.',flines);
+				});
+			} else if (program.check) {
+				console.log('Searching for missing \''+program.check+'\' commits...');
+				exec("git cherry 12.x master -v --abbrev=10", function (error, stdout, stderr) {
+					var flines = processLines(stdout,function(t) { 
+						return t.indexOf('+') == 0 
+							&& t.indexOf(program.check) >= 0;
+					});
+					showLines('Found '+flines.length+' missed commits.',flines);
+				});
+			} else if (program.jira) {
+				var verify = 'verified:merge';
+				var verifyRegExp = /(verified|resolved|closed|any):(merge|resolved|none)/;
+				if (program.jira_verify) {
+					if (program.jira_verify.toLowerCase().match(verifyRegExp)) {
+						verify = program.jira_verify;
+					} else {
+						console.log('Invalid -j options \''+program.jira_verify+'\' using defaults.')
+					}
+				}
+				console.log('Searching for missing jira ['+verify+'] commits...');
+				exec("git cherry 12.x master -v --abbrev=10", function (error, stdout, stderr) {
+					var flines = processLines(stdout,function(t) {
+						return t.indexOf('+') == 0 
+							&& isJiraRelated(t)
+							&& isOnly(t)
+							&& isJiraReady(t,function(){
+								var v = verifyRegExp.exec(verify.toLowerCase());
+								var total = 0;
+								for (var i=0; i<flines.length; i++) {
+									var keys = extractJiraKeys(flines[i])
+									if (areJirasReady(keys,v)) {
+										showLine(flines[i]);
+										total++;
+									}
+								}
+								console.log('Found '+total+' missing ready jira commits')
+							});
+					});
+				});
+			} else if (program.github) {
+				console.log('Searching for missing github issues commits...');
+				exec("git cherry 12.x master -v --abbrev=10", function (error, stdout, stderr) {
+					var flines = processLines(stdout,function(t) {
+						return t.indexOf('+') == 0 
+							&& isGithubRelated(t)
+							&& isOnly(t);
+					});
+					showLines('Found '+flines.length+' missed commits.',flines);
+				});
+			} else if (program.all) {
+				console.log('Searching for global missing commits...');
+				exec("git cherry 12.x master -v --abbrev=10", function (error, stdout, stderr) {
+					var flines = processLines(stdout,function(t) {
+						return t.indexOf('+') == 0 
+							&& isOnly(t);
+					});
+					showLines('Found '+flines.length+' missed commits.',flines);
+				});
+			}
+		});
+	}
+}
+
+
+program
+.version('1.0.0')
+.description('Sakai cherry pick management script')
+.option('-u, --update', 'Update local git working copies')
+.option('-f, --find <find>', 'Find some commit in 12.x that matches pattern')
+.option('-m, --missing','List missing commits in 12.x without jira or github issue reference')
+.option('-c, --check <check>','Look for some missing commits in 12.x that matches pattern')
+.option('-j, --jira [verified|resolved|closed|any:none|merge|resolved]','List missing commits of jiras with status and 11 status as the value specified (default verified:merge)',function(v,s){ program.jira_verify = v; })
+.option('-o, --only <only>','Filter missing jiras that contains pattern, use in addition to other options')
+.option('-g, --github', 'List missing commits of github issues without workflow checking')
+.option('-a, --all', 'List all missing commits without any check')
+.option('-p, --picks', 'Show cherry-pick commands, use in addition to other options')
+.parse(process.argv);
+
+if (program.update) {
+	console.log('Updating local git working copies...');
+	exec("git checkout master;git pull upstream master;git checkout 12.x;git pull upstream 12.x", function (error, stdout, stderr) {
+		if (error) {
+			console.log(stderr);
+		} else {
+			console.log('Success!');
+			processOptions(true);
+		}
+	});	
+} else {
+	if (program.find || program.missing || program.check || program.jira || program.github || program.all) {
+		processOptions(false);
+	} else {
+		console.log('You must choose one option, try --help')
+	}
+}
+
